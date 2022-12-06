e6d0d3214ad35f8206a94eda929fc16fb1f61643
Barış Mete <73601795+IMetex@users.noreply.github.com>
2022-12-06 16:25:06 +0300

    Update Main.java

--- DaireAlanCevreHesaplama/src/Main.java
+++ DaireAlanCevreHesaplama/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         int r;
-        double pi = 3.14 ,alan, cevre;
+        double pi = 3.14 ,alan, cevre, merkez, alanp;
 
         Scanner inp = new Scanner(System.in);
         System.out.print("Dairenin Yarıcapını Giriniz: ");
@@ -14,6 +14,13 @@ public class Main {
         cevre = 2 * pi * r;
         System.out.println("Dairenin Alanı : " + alan);
         System.out.println("Dairenin Cevresi : "+ cevre);
+        
+        System.out.print("Merkez olcusunu giriniz : ");
+        merkez = inp.nextInt();
+
+        alanp = (pi * (r*r)*merkez)/360;
+
+        System.out.println("Daire Diliminin Alani : " + alanp);
 
 
 

b38086559beab8d5ec764d05a2d43f36063031e4
Barış Mete <73601795+IMetex@users.noreply.github.com>
2022-12-06 16:20:52 +0300

    Add files via upload

---
+++ DaireAlanCevreHesaplama/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int r;
+        double pi = 3.14 ,alan, cevre;
+
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Dairenin Yarıcapını Giriniz: ");
+        r = inp.nextInt();
+
+        alan = pi * r * r;
+        cevre = 2 * pi * r;
+        System.out.println("Dairenin Alanı : " + alan);
+        System.out.println("Dairenin Cevresi : "+ cevre);
+
+
+
+    }
+}
